5f0bfd9d69058c9788d78963783ee43eb885ddd3
Lucas <lucaslugui1604@gmail.com>
2025-05-09 10:44:13 -0300

    corrigindo erros

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,25 @@
+//DECLARANDO O ARRAY DE IMAGENS
 
-// ARRAY DE IMAGENS
-let imagens = [
-    'src/assets/montanhas.jpg',
-    'src/assets/praia.jpg',
+let imagens =[
     'src/assets/universo.jpg',
+    'src/assets/praia.jpg',
+    'src/assets/montanhas.jpg',
 ];
 
-let i = 0
-let tempo = 3000
+//DECLARANDO AS VARIAVEIS
 
-// criando função slideshow
-function slideShow() {
-    document.getElementById('image').src=imagens[i];
-    i++;
-    if(i == imagens.length){
-        i = 0;
-        setTimeout('slideShow()', tempo)
+let index=0;
+let tempo=3000;
+
+//CRIANDO A FUNÇÃO SLIDESHOW
+
+function slideShow(){
+    document.getElementById("image").src=imagens[index];
+    index++;
+
+    if(index == imagens.length){
+        index=0;
     }
+    setTimeout("slideShow()",tempo)
 }
+slideShow();
